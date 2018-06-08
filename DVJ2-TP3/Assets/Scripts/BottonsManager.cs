9a02626abaf66b77aa69e888abaa02e54a005b57
Unknown <lean1998@hotmail.com>
2018-06-08 20:37:10 -0300

    Pause Button

---
+++ DVJ2-TP3/Assets/Scripts/BottonsManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottonsManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Pause()
+    {
+        if (Time.timeScale > 0)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}

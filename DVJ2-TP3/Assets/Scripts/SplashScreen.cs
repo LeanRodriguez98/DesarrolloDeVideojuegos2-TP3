3e36aa25eb0d987ab8cec818a46e5735d7efb5c3
Unknown <jquintanamartin@hotmail.com>
2018-06-09 12:52:05 -0300

    Splash Screen delay
    Ahora se queda en pantalla por unos segundos antes de desaparecer

--- DVJ2-TP3/Assets/Scripts/SplashScreen.cs
+++ DVJ2-TP3/Assets/Scripts/SplashScreen.cs
@@ -5,9 +5,15 @@ using UnityEngine.UI;
 
 
 public class SplashScreen : MonoBehaviour {
+    public float Fade = 10;
+    public float wait = 2;
+
     private RawImage Image01 = null;
     private float Alpha;
     private Color NewColor;
+    private bool FadeOut = false;
+    private float time = 0;
+
 	// Use this for initialization
 	void Start () {
         Alpha = 0;
@@ -16,18 +22,30 @@ public class SplashScreen : MonoBehaviour {
         NewColor.r = 255;
         Image01 = this.GetComponent<RawImage>();
 
+        
+
     }
 
     // Update is called once per frame
     void Update () {
-     
-        if (Alpha < 1)
+        
+        if (!FadeOut && Alpha < 1)
         {
-            Alpha += Time.deltaTime / 10;
+            Alpha += Time.deltaTime / Fade;
          
             NewColor.a = Alpha;
             Image01.color = NewColor;
-
+        }
+        else
+        {
+            time += Time.deltaTime;
+            if ( time >= wait)
+            {
+                FadeOut = true;
+                Alpha -= Time.deltaTime / Fade;
+                NewColor.a = Alpha;
+                Image01.color = NewColor;
+            }
         }
     }
 }

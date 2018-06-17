2e3c433f3c15a7d2d67844beb1434f7739ce0635
Unknown <theoleyenda1997@gmail.com>
2018-06-16 21:38:16 -0300

    Personaje En Juego (roto)

---
+++ DVJ2-TP3/Assets/Assets/Scripts/BottonsManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottonsManager : MonoBehaviour {
+    public Ship _Ship;
+    public GameObject Button01;
+    public GameObject Button02;
+    public GameObject Button03;
+    public GameObject LandButton;
+
+    // Use this for initialization
+    void Start () {
+        _Ship = Ship.instance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_Ship.LandPlanetButton)
+        {
+            LandButton.SetActive(true);
+        }
+        else
+        {
+            LandButton.SetActive(false);
+        }
+    }
+
+    public void Pause()
+    {
+        if (Time.timeScale > 0)
+        {
+            Time.timeScale = 0;
+            Button01.SetActive(true);
+            Button02.SetActive(true);
+            Button03.SetActive(true);
+
+        }
+        else
+        {
+            Time.timeScale = 1;
+            Button01.SetActive(false);
+            Button02.SetActive(false);
+            Button03.SetActive(false);
+        }
+    }
+
+    public void Test()
+    {
+        Debug.Log("TEST");
+    }
+
+}
---
+++ DVJ2-TP3/Assets/Assets/Scripts/Planets.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Planets : MonoBehaviour {
+    public GameObject RotateAround;
+    public Rigidbody m_rigidbody;
+
+    private float Radius;
+    public float TraslationSpeed;
+    public float Angle;
+    public float RotationSpeed;
+
+    // Use this for initialization
+    void Start () {
+
+		Radius = Mathf.Sqrt((transform.position.x - RotateAround.transform.position.x *
+            transform.position.x - RotateAround.transform.position.x) + 
+            (transform.position.z - RotateAround.transform.position.x *
+            transform.position.z - RotateAround.transform.position.x));
+        TraslationSpeed += Random.Range(-5, 5);
+        if (TraslationSpeed <= 0)
+            TraslationSpeed = 1;
+        m_rigidbody = GetComponent<Rigidbody>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!Ship.instance.stopPlanets)
+        {
+            Angle += TraslationSpeed * Time.deltaTime;
+            Vector3 newPos = Vector3.zero;
+            newPos.x = RotateAround.transform.position.x + Radius * Mathf.Cos(Angle * Mathf.Deg2Rad);
+            newPos.z = RotateAround.transform.position.z + Radius * Mathf.Sin(Angle * Mathf.Deg2Rad);
+            transform.position = newPos;
+            transform.Rotate(transform.up * RotationSpeed * Time.deltaTime);
+           
+        }
+        else
+        {
+            m_rigidbody.constraints = RigidbodyConstraints.FreezeRotationX |
+                                      RigidbodyConstraints.FreezeRotationY |
+                                      RigidbodyConstraints.FreezeRotationZ;
+
+        }
+
+    }
+}

85a4d29f20a400d7ade692ffd05b5c700b9f00ab
Unknown <theoleyenda1997@gmail.com>
2018-06-19 16:35:57 -0300

    Movement player - Beta

---
+++ DVJ2-TP3/Assets/Assets/Scripts/AnimatorControlledPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DVJ02.Clase08
+{
+public class AnimatorControlledPlayer : MonoBehaviour
+{
+    private Animator animator;
+    // Use this for initialization
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+ 
+            animator.SetFloat("Speed", Mathf.Abs(horizontal));
+            animator.SetFloat("Speed", Mathf.Abs(vertical));
+
+            //animator.SetFloat("Speed", 1); si vale 1 camina
+            //animator.SetFloat("Speed", 0); si vale 0 se queda quieto
+
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetButton("Fire1"))
+            {
+                animator.SetTrigger("Attack");
+            }
+    }
+}
+}
---
+++ DVJ2-TP3/Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+// HACER UN CODIGO DE MOVIMIENTO PROPIO; ESTE NO SIRVE
+namespace DVJ02.Clase08
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private bool moving;
+        protected Rigidbody rig;
+        public float speed;
+        private char lastKey;
+        void Start()
+        {
+            rig = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        { 
+            Move();
+        }
+
+        private void Move()
+        {
+            float vertical = Input.GetAxis("Vertical");
+            transform.Translate(0, 0, vertical*speed);
+            float horizontal = Input.GetAxis("Horizontal");
+            transform.Rotate(0, horizontal, 0);
+        }
+    }
+}

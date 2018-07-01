54e6f2cc9ea9406040e20db48cf555060d140d1e
Unknown <jquintanamartin@hotmail.com>
2018-07-01 17:38:05 -0300

    Equipment

---
+++ DVJ2-TP3/Assets/Assets/Scripts/PlanetGameplay/Equipment/EquipmentManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentManager : MonoBehaviour {
+
+    #region Singleton
+
+    public static EquipmentManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(instance);
+            return;
+        }
+        instance = this;
+    }
+    #endregion
+
+    public Equipment[] currentEquipment;
+
+    private void Start()
+    {
+        int cantSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
+        currentEquipment = new Equipment[cantSlots];
+    }
+
+    public void Equip(Equipment newEquipment){
+        currentEquipment[(int)newEquipment.slot] = newEquipment;
+    }
+}
---
+++ DVJ2-TP3/Assets/Assets/Scripts/PlanetGameplay/Items/Equipment.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public enum EquipmentSlot
+{
+    Head,
+    Chest,
+    Weapon,
+    Shield,
+    Legs
+}
+
+[CreateAssetMenu(fileName = "NewItem", menuName = "Inventory/Equipment")]
+public class Equipment : Item {
+
+    public EquipmentSlot slot;
+
+    public int armorModiefier;
+    public int damageModifier;
+
+    public override void Use()
+    {
+        EquipmentManager.instance.Equip(this);
+        Debug.Log("Equiping " + name);
+    }
+}
